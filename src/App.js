4570b0e9b13b5ec13df0059c8b7ea10f7f73dd33
James Leaverland <landjleaverland@gmail.com>
2025-12-09 19:10:49 +0000

    Just a form now

--- src/App.js
+++ src/App.js
@@ -26,18 +26,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Did this work?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <form onSubmit={handleSubmit}>
           <label htmlFor="name">Name:</label>
           <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} /><br/><br/>
