cdca6d8301e10a560aec383e8af5cb5e796d7b6a
James Leaverland <landjleaverland@gmail.com>
2025-12-10 22:05:03 +0000

    Start adding climbing center select option

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
+      [e.target.climbing_center]: e.target.value
     });
   };
 
@@ -27,8 +27,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <form onSubmit={handleSubmit}>
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} /><br/><br/>
+          <label htmlFor="climbing_center">Climbing Center: </label>
+          <select 
+            id="climbing_center" 
+            name="climbing_center" 
+            value={formData.climbing_center} 
+            onChange={handleChange}
+            style={{ height: '40px', width: '100%' }}
+          >
+            <option value="">Select climbing center</option>
+            <option value="John">Flashpoint Swindon</option>
+            <option value="Jane">Flashpoint Bristol</option>
+            <option value="Bob">Rockstar Swindon</option>
+            <option value="Bob">Redpoint Bristol</option>
+          </select><br/><br/>
 
           <label htmlFor="email">Email:</label>
           <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} /><br/><br/>
@@ -47,3 +59,8 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
