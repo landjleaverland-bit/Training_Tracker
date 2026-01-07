00b8460f58c4ee0ae669ea41476df7f32c1bbf80
James Leaverland <landjleaverland@gmail.com>
2026-01-07 22:40:38 +0000

    Updates

--- _app/immutable/chunks/BmImkCJR.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1scncds?.base??"/Training_Tracker",a=globalThis.__sveltekit_1scncds?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/h4Oy4OB0.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_rh7f3n?.base??"/Training_Tracker",a=globalThis.__sveltekit_rh7f3n?.assets??s??"";export{a,s as b};
