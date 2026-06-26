859fbd34daf617eb351a3463f910c1471291995c
James Leaverland <landjleaverland@gmail.com>
2026-06-26 15:47:22 +0100

    Updates

---
+++ _app/immutable/nodes/2.CLBAxDyx.js
@@ -0,0 +1,1 @@
+import{f as t,a as r}from"../chunks/iSXWTlgC.js";import{i as s}from"../chunks/CM4oN1wi.js";import{p as i,o as e,a as m}from"../chunks/BFZxL6-c.js";import{g as n,b as f}from"../chunks/DN5hSeG1.js";var g=t('<div class="loading svelte-1uha8ag"><p>Loading...</p></div>');function c(a,o){i(o,!1),e(()=>{n(`${f}/log`,{replaceState:!0})}),s();var p=g();r(a,p),m()}export{c as component};
